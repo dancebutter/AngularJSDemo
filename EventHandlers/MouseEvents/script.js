4b976794cd7a77f64da331dca595b2d07f89b948
dancebutter <502381314@HCU-BS6TH12.clients.am.health.ge.com>
2014-12-30 16:40:07 -0800

    add Mouse Down, Up, Enter, Leave, Move, Over(Bug need to fix)

--- EventHandlers/MouseEvents/script.js
+++ EventHandlers/MouseEvents/script.js
@@ -3,26 +3,64 @@ angular.module("mainModule", [])
 		$scope.onFirstBtnClickResult = "";
 		$scope.secondBtnInput = "";
 		$scope.onDblClickResult = "";
+		$scope.onMouseDownResult = "";
+		$scope.onMouseUpResult = "";
+		$scope.onMouseEnterResult = "";
+		$scope.onMouseLeaveResult = "";
+		$scope.onMouseMoveResult = "";
+		$scope.onMouseOverResult = "";
 
-		//
-		// var getCrossBrowserElementCoords = function(mouseEvent) {
-		// 	var result = {
-		// 		x: 0,
-		// 		y: 0
-		// 	};
 
-		// 	if(!mouseEvent) {
-		// 		mouseEvent = window.event;
-		// 	}
 
-		// 	return result;
-		// };
+		// Get mouse event
+		var getMouseEventResult = function(mouseEvent, mouseEventDesc) {
+			var coords = getCrossBrowserElementCoords(mouseEvent);
+			return mouseEventDesc + " at ( " + coords.x + ", " + coords.y + " ) ";
+		};
 
-		// var getMouseEventResult = function(mouseEvent, mouseEventDesc) {
-		// 	// body...
-		// 	var coords = getCrossBrowserElementCoords(mouseEvent);
-		// 	return mouseEventDesc + " at (" + coords.x + ", " + coords.y + ")";
-		// };
+		// Utility function
+		// Accepts a MouseEvent as input and returns the x and y
+		// coordinates relative to the target elements.
+		var getCrossBrowserElementCoords = function(mouseEvent) {
+			var result = {
+				x: 0,
+				y: 0
+			};
+
+			if(!mouseEvent) {
+				mouseEvent = window.event;
+			}
+
+			if(mouseEvent.pageX || mouseEvent.pageY) {
+				result.x = mouseEvent.pageX;
+				result.y = mouseEvent.pageY;
+			} else if(mouseEvent.clientX || mouseEvent.clientY) {
+				result.x = mouseEvent.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
+				result.y = mouseEvent.clientY + document.body.scrollTop + document.documentElement.scrollTop;
+			}
+
+			if(mouseEvent.target) {
+				var offEl = mouseEvent.target;
+				var offX = 0;
+				var offY = 0;
+
+				if(typeof(offEl.offsetParent) != "underfined") {
+					while(offEl) {
+						offX += offEl.offsetLeft;
+						offY += offEl.offsetTop;
+
+						offEl + offEl.offsetParent;
+					}
+				} else {
+					offX = offEl.x;
+					offY = offEl.y;
+				}
+
+				result.x -= offX;
+				result.y -= offY;
+			}
+			return result;
+		};
 
 		//Event handlers
 		$scope.onFirstBtnClick = function() {
@@ -33,5 +71,23 @@ angular.module("mainModule", [])
 		};
 		$scope.onDblClick = function() {
 			$scope.onDblClickResult = "DOUBLE-CLICKED";
+		};
+		$scope.onMouseDown = function($event) {
+			$scope.onMouseDownResult = getMouseEventResult($event, "Mouse down");
+		};
+		$scope.onMouseUp = function($event) {
+			$scope.onMouseUpResult = getMouseEventResult($event, "Mouse up");
+		};
+		$scope.onMouseEnter = function($event) {
+			$scope.onMouseEnterResult = getMouseEventResult($event, "Mouse Enter");
+		};
+		$scope.onMouseLeave = function($event) {
+			$scope.onMouseLeaveResult = getMouseEventResult($event, "Mouse Leave");
+		};
+		$scope.onMouseMove = function($event) {
+			$scope.onMouseMoveResult = getMouseEventResult($event, "Mouse Move");
+		};
+		$scope.onMouseOver = function($event) {
+			$scope.onMouseOverResult = getMouseEventResult($event, "Mouse Over");
 		};
 	});
