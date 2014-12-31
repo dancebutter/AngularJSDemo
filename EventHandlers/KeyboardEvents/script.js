b35f8dd2ad1ba2e633cb66366269c24f128501c3
dancebutter <ken.zhangtq@gmail.com>
2014-12-30 22:04:33 -0800

    add Keyboard Events

---
+++ EventHandlers/KeyboardEvents/script.js
@@ -0,0 +1,24 @@
+angular.module("mainModule", [])
+	.controller("mainController", function($scope) {
+		// Initialization
+		$scope.onKeyDownResult = "";
+		$scope.onKeyUpResult = "";
+		$scope.onKeyPressResult = "";
+
+		// Utility function
+
+		var getKeyboardEventResult = function(keyEvent, keyEventDesc) {
+			return keyEventDesc + " (keyCode: " + (window.event ? keyEvent.keyCode : keyEvent.which) + ") ";
+		};
+		
+		// Event handlers
+		$scope.onKeyDown = function($event) {
+			$scope.onKeyDownResult = getKeyboardEventResult($event, "Key Down");
+		};
+		$scope.onKeyUp = function($event) {
+			$scope.onKeyUpResult = getKeyboardEventResult($event, "Key Up");
+		};
+		$scope.onKeyPress = function($event) {
+			$scope.onKeyPressResult = getKeyboardEventResult($event, "Key Press");
+		};
+	});
