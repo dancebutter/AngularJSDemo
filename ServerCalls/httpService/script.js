3e4d8e03390299a35e35b8d3c2f6e3bc5d996332
dancebutter <ken.zhangtq@gmail.com>
2015-01-06 20:58:29 -0800

    doing http service #4, bug in server.php

--- ServerCalls/httpService/script.js
+++ ServerCalls/httpService/script.js
@@ -38,4 +38,71 @@ angular.module("mainModule", [])
 				  $scope.getCallResult = logResult("GET ERROR", data, status, headers, config);
 				});
 		};
+
+		$scope.getCallJSON = function() {
+			var params = {
+				jsonObjParam: {
+					param1: $scope.getJSONParam1,
+					param2: $scope.getJSONParam2
+				}
+			};
+
+			var config = {
+				params: params
+			};
+
+			$http.get("server.php", config)
+				.success(function(data, status, headers, config) {
+					data = jsonFilter(data);
+
+					$scope.getCallJSONResult = logResult("GET SUCCESS", data, status, headers, config);
+				})
+				.error(function(data, status, headers, config) {
+					$scope.getCallJSONResult = logResult("GET ERROR", data, status, headers, config);
+				});
+		};
+
+		$scope.getCallJSONFile = function() {
+			var params = {
+				jsonObjParam: {
+					param1: $scope.getJSONParam1,
+					param2: $scope.getJSONParam2
+				}
+			};
+
+			var config = {
+				params: params
+			};
+
+			$http.get("temp.json", config)
+				.success(function(data, status, headers, config) {
+
+					$scope.getCallJSONFileResultOnly = data;
+
+					data = jsonFilter(data);
+					$scope.getCallJSONFileResult = logResult("GET SUCCESS", data, status, headers, config);
+				})
+				.error(function(data, status, headers, config) {
+					$scope.getCallJSONFileResult = logResult("GET ERROR", data, status, headers, config);
+				});
+		};
+
+		$scope.getCallTimeout = function() {
+			var params = {
+				sleep: 3 // sleep for 3 second before responding
+			};
+
+			var config = {
+				params: params,
+				timeout: 1000 // wait at most 1 second for the response
+			};
+
+			$http.get("server.php", config)
+				.success(function(data, status, headers, config) {
+					$scope.getCallTimeoutResult = data;
+				})
+				.error(function(data, status, headers, config) {
+					console.log("----------------------error----------------");
+				});
+		};
 	});
